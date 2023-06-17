74dbe345b995ef684b7aa2f4395441e1b0247824
HICHAMSEBAA <hichamsebaa45@gmail.com>
2023-06-17 22:24:58 +0100

    add view of update student and controller class

---
+++ src/main/java/library/updateStudent/UpdateStudentController.java
@@ -0,0 +1,20 @@
+package library.updateStudent;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+public class UpdateStudentController{
+
+    @FXML
+    private void cancel(ActionEvent actionEvent){
+
+    }
+
+    @FXML
+    private void update(ActionEvent actionEvent){
+
+    }
+
+}
