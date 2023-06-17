027611aaf8ef613c31db97cc358627600f7b1850
HICHAMSEBAA <hichamsebaa45@gmail.com>
2023-06-17 22:23:54 +0100

    add view of add student and controller class

---
+++ src/main/java/library/addStudent/AddStudentController.java
@@ -0,0 +1,12 @@
+package library.addStudent;
+
+import javafx.event.ActionEvent;
+
+public class AddStudentController {
+
+    public void save(ActionEvent actionEvent) {
+    }
+
+    public void cancel(ActionEvent actionEvent) {
+    }
+}
