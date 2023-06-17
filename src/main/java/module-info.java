a8017ab8fdd4d7b37a9a242c7786b1eedcec6e11
HICHAMSEBAA <hichamsebaa45@gmail.com>
2023-06-17 22:29:30 +0100

    ...

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,16 @@ module library.library {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens library.library to javafx.fxml;
     exports library.library;
+    opens library.addBook to javafx.fxml;
+    exports library.addBook;
+    opens library.updateBook to javafx.fxml;
+    exports library.updateBook;
+    opens library.addStudent to javafx.fxml;
+    exports library.addStudent;
+    opens library.updateStudent to javafx.fxml;
+    exports library.updateStudent;
 }
