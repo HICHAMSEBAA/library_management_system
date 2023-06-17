7cea19bc1c2cab329fac1a985368957f3d8f2e36
HICHAMSEBAA <hichamsebaa45@gmail.com>
2023-06-17 22:24:26 +0100

    add view of update book and controller class

---
+++ src/main/java/library/updateBook/UpdateBookController.java
@@ -0,0 +1,12 @@
+package library.updateBook;
+
+import javafx.event.ActionEvent;
+
+public class UpdateBookController {
+
+    public void update(ActionEvent actionEvent) {
+    }
+
+    public void cancel(ActionEvent actionEvent) {
+    }
+}
